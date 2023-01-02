6cb902c3412201689640d2ad4a36f725e001833c
wandergithub <wandergonzalez0@hotmail.com>
2023-01-02 15:47:32 -0400

    Create static view root#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   
   # Selects a random greeting from table
-  get '/message', to: 'message#index'
+  # get '/message', to: 'message#index'
 end
